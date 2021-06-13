fd45b4b4264f4c1703bbc24443c590551256080d
Pradyumna Hebbar <pradyu.hebbar@gmail.com>
2021-06-13 13:30:24 +0530

    Initial Commit

---
+++ src/components/CountrySelect.vue
@@ -0,0 +1,36 @@
+<template>
+  <select
+    @change="onChange()"
+    v-model="selected"
+    class="form-select mt-10 block w-full border p-3 rounded"
+  >
+    <option value="0">Select Country</option>
+    <option
+      :key="country.ID"
+      v-for="country in countries"
+      :value="country.ID"
+      >{{ country.Country }}</option
+    >
+  </select>
+</template>
+
+<script>
+export default {
+  name: "CountrySelect",
+  props: ["countries"],
+  data() {
+    return {
+      selected: 0,
+    };
+  },
+  methods: {
+    onChange() {
+      const country = this.countries.find(
+        (country) => country.ID === this.selected
+      );
+
+      this.$emit("get-country", country);
+    },
+  },
+};
+</script>
